beae6ee15c10411a736b2f1cb002acc888a215f4
SlavaKuntsevich <mactep_chif@mail.ru>
2023-02-06 22:43:34 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)           
+        {   //ЗАДАЧА #1
+            //    Console.WriteLine("Введите номер месяца:");
+            //    int m=int.Parse(Console.ReadLine());
+            //    switch (m) 
+            //    {
+            //        case 1 : Console.WriteLine("Зима");
+            //            break;
+            //            case 2 : Console.WriteLine("Зима");
+            //            break;
+            //            case 3 : Console.WriteLine("Весна");
+            //            break;
+            //            case 4 : Console.WriteLine("Весна");
+            //            break;
+            //            case 5 : Console.WriteLine("Весна");
+            //            break;  
+            //            case 6 : Console.WriteLine("Лето");
+            //            break;
+            //            case 7 : Console.WriteLine("Лето");
+            //            break;
+            //            case 8 : Console.WriteLine("Лето");
+            //            break;
+            //            case 9 : Console.WriteLine("Осень");
+            //            break;
+            //            case 10 : Console.WriteLine("Осень");
+            //            break;
+            //            case 11 : Console.WriteLine("Осень");
+            //            break;
+            //            case 12 : Console.WriteLine("Зима");
+            //            break;
+            //    }
+            // ЗАДАЧА #2
+            //Console.WriteLine("Введите номер месяца:");
+            //int R=int.Parse(Console.ReadLine());
+            //if (R >0 && R <3 || R== 12) {
+            //    Console.WriteLine("Зима");
+            //        }
+            //if (R >2 && R<6) {
+            //    Console.WriteLine("Весна");
+            //        }
+            //if (R >5 && R<9) {
+            //    Console.WriteLine("Лето");
+            //}
+            //if (R >8&& R<12) {
+            //    Console.WriteLine("Осень");
+            //}
+            //ЗАДАЧА #3
+            // Console.WriteLine("Введите число:");
+            // int q=int.Parse (Console.ReadLine());
+            // Convert.ToInt32(q);
+            //if (q %2 ==0) {
+            //     Console.WriteLine("Число четное");
+            //         }
+            //else { Console.WriteLine("Число нечетное");
+            // }
+
+
+
+
+            // ЗАДАЧА #4 
+            //Console.WriteLine("Введите температуру: ");
+            //int t=int.Parse(Console.ReadLine());
+            //if (t >-5 ) {
+            //    Console.WriteLine("Тепло");
+            //        }
+            //if (t <-5&& t >-20) {
+            //    Console.WriteLine("Нормально");
+            //}
+            //if (t <-20) {
+            //    Console.WriteLine("Холодно");
+            //        }
+            
+
+
+
+           //ЗАДАЧА #5
+            //Console.WriteLine("Введите число от 1 до 7 :");
+            //string t = Console.ReadLine();
+            //Convert.ToInt32(t);
+            //if (t == "1")
+            //{
+            //    Console.WriteLine("Красный");
+            //}
+            //if (t == "2")
+            //{
+            //    Console.WriteLine("Оранжевый");
+
+            //}
+            //if (t == "3")
+            //{
+            //    Console.WriteLine("Желтый");
+
+            //}
+            //if (t == "4")
+            //{
+            //    Console.WriteLine("Зеленый");
+
+            //}
+            //if (t == "5")
+            //{
+            //    Console.WriteLine("Голубой");
+
+            //}
+            //if (t == "6")
+            //{
+            //    Console.WriteLine("Синий");
+            //}
+            //if (t == "7")
+            //{
+            //    Console.WriteLine("Фиолетовый");
+            //}
+
+
+
+        }
+    }
+}
